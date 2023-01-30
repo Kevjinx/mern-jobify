2eedd706756498eabada92573dead2d4306eea4c
kevjinx <kevin415mei@gmail.com>
2023-01-30 11:43:30 -0800

    add style main wrapper

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import logo from './assets/images/logo.svg'
-import main from './assets/images/main.svg'
+import mainImg from './assets/images/main.svg'
 import styled from 'styled-components'
 
 
 const Landing = () => {
 	return (
-		<main>
+		<Wrapper>
 			<nav>
 				<img src={logo} alt='logo' className='logo' />
 			</nav>
@@ -21,11 +21,50 @@ const Landing = () => {
 					<button className='btn btn-hero'>Login/Register</button>
 				</div>
 				<div className='image'>
-					<img src={main} alt='job hunt' className='img main-img'/>
+					<img src={mainImg} alt='job hunt' className='img main-img'/>
 				</div>
 			</div>
-		</main>
+		</Wrapper>
 	)
 }
+
+const Wrapper = styled.main`
+	nav {
+		width: var(--fluid-width);
+		max-width: var(--max-width);
+		margin: 0 auto;
+		height: var(--nav-height);
+		display: flex;
+		align-items: center;
+	}
+	.page {
+		min-height: calc(100vh - var(--nav-height));
+		display: grid;
+		align-items: center;
+		margin-top: 2rem;
+	}
+	h1{
+		font-weight: 700;
+		span {
+			/* you can NEST!!!! */
+			color: var(--primary-500);
+		}
+	}
+	p {
+		color: var(--gray-600);
+	}
+	.main-img {
+		display: none;
+	}
+	@media (min-width: 1000px) {
+		.page {
+			grid-template-columns: 1fr 1fr;
+			column-gap: 3rem;
+		}
+		.main-img {
+			display: block;
+		}
+	}
+`
 
 export default Landing
